4ab6f31aaf185e2baba25185de4137842030a015
Alae Ez-zaouine <aez-zaou@e2r5p8.1337.ma>
2021-01-27 18:02:25 +0100

    done with ssl

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+echo -e "bilal@1999\nbilal@1999" | adduser -h / alae
 echo "<h1>hello world</h1>" >> /var/www/index.html
 rm /etc/nginx/conf.d/default.conf
 mv default.conf /etc/nginx/conf.d/
 service nginx start
+service sshd start
