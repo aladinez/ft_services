f07614097f167f8ef95ceeb14864d2a3fe697a85
Alae Ez-zaouine <aez-zaou@student.1337.ma>
2021-02-15 18:59:14 +0100

    fixed mysql, working on data persistence..

--- build-images.sh
+++ build-images.sh
@@ -8,13 +8,13 @@ sed -i '' s/ALADINLEPRINCE/$(minikube ip)/g influxdb/telegraf.conf
 sed -i '' s/ALADINLEPRINCE/$(minikube ip)/g ftps/srcs/vsftpd.conf
 
 
-docker build -t nginx-image nginx --no-cache
-docker build -t grafana-image grafana --no-cache
+#docker build -t nginx-image nginx --no-cache
+#docker build -t grafana-image grafana --no-cache
 docker build -t wordpress-image wordpress --no-cache
 docker build -t phpmyadmin-image phpmyadmin --no-cache
 docker build -t mysql-image mysql --no-cache
-docker build -t influxdb-image influxdb --no-cache
-docker build -t ftps-image ftps --no-cache
+#docker build -t influxdb-image influxdb --no-cache
+#docker build -t ftps-image ftps --no-cache
 
 sed -i '' s/$(minikube ip)/ALADINLEPRINCE/g mysql/db_wordpress.sql
 sed -i '' s/$(minikube ip)/ALADINLEPRINCE/g yaml/metallbConfigmap.yaml
--- mysql/run.sh
+++ mysql/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rc
+/usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 service mariadb start
 service telegraf start
 sh /mysql.sh
