5b50e86b34ed088e9849def29556bf2f1bba5262
Alae Ez-zaouine <aez-zaou@student.1337.ma>
2021-02-16 18:16:01 +0100

    data persistence is working .. mysql service doesnt start after deleting the pod

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 sed -i '' s/ALADINLEPRINCE/$(minikube ip)/g yaml/metallbConfigmap.yaml
 kubectl apply -f yaml/metallbConfigmap.yaml
-# kubectl apply -f yaml/influxdb-deployment.yaml
+kubectl apply -f yaml/influxdb-deployment.yaml
 kubectl apply -f yaml/wordpress-deployment.yaml
-# kubectl apply -f yaml/grafana-deployment.yaml
+kubectl apply -f yaml/grafana-deployment.yaml
 kubectl apply -f yaml/phpmyadmin-deployment.yaml
-# kubectl apply -f yaml/nginx-deployment.yaml
+kubectl apply -f yaml/nginx-deployment.yaml
 kubectl apply -f yaml/mysql-deployment.yaml
-# kubectl apply -f yaml/ftps-deployment.yaml
+kubectl apply -f yaml/ftps-deployment.yaml
 
 
 sed -i '' s/$(minikube ip)/ALADINLEPRINCE/g yaml/metallbConfigmap.yaml
--- mysql/run.sh
+++ mysql/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-rc
+rc-status
 /usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 service mariadb start
 service telegraf start
-sh /mysql.sh
+
+
+user="root"
+dbname="db_wordpress"
+name="admin"
+pass="admin"
+
+mysql -u $user -e "CREATE DATABASE $dbname;CREATE USER $name@'%' identified by '$pass';GRANT ALL ON *.* to $name@'%' WITH GRANT OPTION;FLUSH PRIVILEGES;"
+#grant all tables on all databases to $name , with ability to grant any other user (with grant optin).
+
+mysql < /db_wordpress.sql -u root db_wordpress
+
+tail -f /dev/null
