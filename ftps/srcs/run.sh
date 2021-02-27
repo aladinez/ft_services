6ac88d0b4f441ac5580805b254dfd3570d4ddea2
Alae Ez-zaouine <aez-zaou@student.1337.ma>
2021-02-27 18:12:09 +0100

    fixed the crushing servies, fixed grafana databases, working on livneness..

--- ftps/srcs/run.sh
+++ ftps/srcs/run.sh
@@ -6,7 +6,6 @@ echo -e "123456789\n123456789" | adduser alae
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem \
 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.alae.com"
-
 service vsftpd start
 service telegraf start
 tail -f /dev/null
